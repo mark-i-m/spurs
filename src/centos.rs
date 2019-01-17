bc418805c764200e30069a831414d0490fc67430
Mark Mansi <markm@cs.wisc.edu>
2019-01-17 14:09:19 -0600

    add a few tests

--- src/centos.rs
+++ src/centos.rs
@@ -3,11 +3,46 @@
 use crate::ssh::SshCommand;
 
 /// Install the given .rpm packages via `rpm`. Requires `sudo` priveleges.
-pub fn dpkg_install(pkg: &str) -> SshCommand {
+pub fn rpm_install(pkg: &str) -> SshCommand {
     cmd!("sudo rpm -ivh {}", pkg)
 }
 
 /// Install the given list of packages via `yum install`. Requires `sudo` priveleges.
 pub fn yum_install(pkgs: &[&str]) -> SshCommand {
     cmd!("sudo yum install -y {}", pkgs.join(" "))
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ssh::SshCommand;
+
+    #[test]
+    fn test_rpm_install() {
+        assert_eq!(
+            super::rpm_install("foobar"),
+            SshCommand::make_cmd(
+                "sudo rpm -ivh foobar".into(),
+                None,
+                false,
+                false,
+                false,
+                false,
+            ),
+        );
+    }
+
+    #[test]
+    fn test_yum_install() {
+        assert_eq!(
+            super::yum_install(&["foobar"]),
+            SshCommand::make_cmd(
+                "sudo yum install -y foobar".into(),
+                None,
+                false,
+                false,
+                false,
+                false,
+            ),
+        );
+    }
 }
