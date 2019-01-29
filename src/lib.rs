dab6f8937e002102853aa1026d45ebdd6fada925
Mark Mansi <markm@cs.wisc.edu>
2019-01-28 20:48:45 -0600

    add html_root_url

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! This is a library containing a bunch of routines that I have found useful for setting up and
 //! running experiments remotely.
 
+#![doc(html_root_url = "https://docs.rs/spurs/0.3.0")]
+
 #[macro_use]
 pub mod ssh;
 
