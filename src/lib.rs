de69caadf1c9ff016ffe90aa6b0572f959411d73
Mark Mansi <markm@cs.wisc.edu>
2019-05-16 13:39:46 -0500

    update crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! This is a library containing a bunch of routines that I have found useful for setting up and
 //! running experiments remotely.
 
-#![doc(html_root_url = "https://docs.rs/spurs/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/spurs/0.4.0")]
 
 #[macro_use]
 pub mod ssh;
