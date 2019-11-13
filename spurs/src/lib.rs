f87a5fd29cea95f9e27b791a2e1ad85d8c3af14d
Mark Mansi <markm@cs.wisc.edu>
2019-11-13 15:42:52 -0600

    remove all remaining utilities from spurs

--- spurs/src/lib.rs
+++ spurs/src/lib.rs
@@ -1,9 +1,8 @@
 //! This is a library containing a bunch of routines that I have found useful for setting up and
 //! running experiments remotely.
 
-#![doc(html_root_url = "https://docs.rs/spurs/0.6.0")]
+#![doc(html_root_url = "https://docs.rs/spurs/0.7.0")]
 
 #[macro_use]
 pub mod ssh;
 pub mod errors;
-pub mod util;
